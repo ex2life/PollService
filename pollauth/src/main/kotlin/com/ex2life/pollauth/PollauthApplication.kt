96e45f8c7eeb0f85cdf717c64c8d899f2ea0b871
ex2life <abramizsaransk@gmail.com>
2020-03-16 21:36:14 +0300

    fix naming

--- pollauth/src/main/kotlin/com/ex2life/pollauth/PollauthApplication.kt
+++ pollauth/src/main/kotlin/com/ex2life/pollauth/PollauthApplication.kt
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 class PollauthApplication
 
 fun main(args: Array<String>) {
