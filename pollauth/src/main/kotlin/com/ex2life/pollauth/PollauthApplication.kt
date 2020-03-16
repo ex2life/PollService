1df14ced6e68ad6f0d19ee19b7288030feef551f
ex2life <abramizsaransk@gmail.com>
2020-03-16 21:05:54 +0300

    initial poll auth microservice

---
+++ pollauth/src/main/kotlin/com/ex2life/pollauth/PollauthApplication.kt
@@ -0,0 +1,11 @@
+package com.ex2life.pollauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PollauthApplication
+
+fun main(args: Array<String>) {
+	runApplication<PollauthApplication>(*args)
+}
---
+++ pollauth/src/test/kotlin/com/ex2life/pollauth/PollauthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ex2life.pollauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PollauthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
