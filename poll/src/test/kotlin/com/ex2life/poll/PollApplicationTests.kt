bfdd8b0dc939a8d8684d67ff751da41ca6ea36bc
ex2life <abramizsaransk@gmail.com>
2020-03-16 21:04:52 +0300

    initial poll microservice

---
+++ poll/src/test/kotlin/com/ex2life/poll/PollApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ex2life.poll
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PollApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
