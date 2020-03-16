55ec51d624284d66b614b0ecd6d6f010bf0edbd5
ex2life <abramizsaransk@gmail.com>
2020-03-16 21:31:07 +0300

    fix feign klient

--- poll/src/main/kotlin/com/ex2life/poll/PollApplication.kt
+++ poll/src/main/kotlin/com/ex2life/poll/PollApplication.kt
@@ -2,8 +2,12 @@ package com.ex2life.poll
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 class PollApplication
 
 fun main(args: Array<String>) {
--- pollauth/src/main/kotlin/com/ex2life/pollauth/PollauthApplication.kt
+++ pollauth/src/main/kotlin/com/ex2life/pollauth/PollauthApplication.kt
@@ -2,6 +2,8 @@ package com.ex2life.pollauth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 class PollauthApplication
